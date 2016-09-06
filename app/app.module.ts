d02affc1e89a6b0bc2f697a24740cf6d52b5937f
hunter202 <swastik.mittaloff@gmail.com>
2016-09-06 21:11:23 +0530

    working perfectly

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
+import { AppComponent }  from './src/app.component';
+import { HttpModule }    from '@angular/http';
+}
   
 @NgModule({
     imports:      [ BrowserModule ],
